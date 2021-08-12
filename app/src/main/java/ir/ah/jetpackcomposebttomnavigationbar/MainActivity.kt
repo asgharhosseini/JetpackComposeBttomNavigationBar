fb2df32e3814db747c80b0392e7e831f710bb94d
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-12 18:28:41 +0430

    Initial commit

---
+++ app/src/main/java/ir/ah/jetpackcomposebttomnavigationbar/MainActivity.kt
@@ -0,0 +1,44 @@
+package ir.ah.jetpackcomposebttomnavigationbar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import ir.ah.jetpackcomposebttomnavigationbar.ui.theme.*
+import ir.ah.jetpackcomposebttomnavigationbar.ui.theme.screen.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeBttomNavigationBarTheme {
+               
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeBttomNavigationBarTheme {
+        Greeting("Android")
+    }
+}
