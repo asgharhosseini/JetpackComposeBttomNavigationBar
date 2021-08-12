1b90e006bca6d5aa2b5dfd891c0f2e752edc87ff
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-12 18:30:13 +0430

    add FavouritesScreen.kt

--- app/src/main/java/ir/ah/jetpackcomposebttomnavigationbar/MainActivity.kt
+++ app/src/main/java/ir/ah/jetpackcomposebttomnavigationbar/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeBttomNavigationBarTheme {
-               
+
 
             }
         }
